3eda9f27e511105450e0158b1fd5d94c6cbeff67
Iana Postolachi <iana@shouter.app>
2023-02-13 10:31:26 +0100

    More inserts from Iana

--- IanasInserts.sql
+++ IanasInserts.sql
@@ -1,3 +1,5 @@
+set search_path = amazon_book_store;
+
 insert into Book(ISBN, title, description, rating, language)
 values (9781338801910, 'Dog Man: Twenty Thousand Fleas Under the Sea', 'A book for curious children', 4.6, 'English'),
        (9780385376716, 'The Wonderful Things You Will Be', 'A goodnight sleep book', 4.1, 'English'),
@@ -6,9 +8,31 @@ values (9781338801910, 'Dog Man: Twenty Thousand Fleas Under the Sea', 'A book f
        (9780147515599, 'Dragons Love Tacos', 'A valentines day book, but for children', 3.8, 'English');
 
 insert into Customer(firstName, lastName, middleName, city, zipCode, country, street, streetNumber)
-values ('Sarah','Lockwood','Bianka','Balti','3101','Moldova','Stefan cel MAre si Sfant',13),
-       ('Alina','Johansen', null,'Alborg','9000','Denmark','Odinsgade',43),
-       ('Ralitsa','Petcova','Tina','Gdańsk','80-007','Poland','Szeroka',22);
+values ('Sarah', 'Lockwood', 'Bianka', 'Balti', '3101', 'Moldova', 'Stefan cel MAre si Sfant', 13),
+       ('Alina', 'Johansen', null, 'Alborg', '9000', 'Denmark', 'Odinsgade', 43),
+       ('Ralitsa', 'Petcova', 'Tina', 'Gdańsk', '80-007', 'Poland', 'Szeroka', 22);
 
 insert into Category(name, parentCategoryId)
-values ('Chidlrens books',null )
+values ('Chidlrens books', null),
+       ('From 6 to 8 years', 2);
+
+insert into genre(id, name)
+values (1, 'Children''s Humorous Comics'),
+       (2, 'Graphic Novels');
+
+insert into author(firstname, lastname, middlename)
+values ('Dav', 'Pilkey', null),
+       ('Emily', 'Martin', 'Winfield'),
+       ('Suzanne', 'Lang', null),
+       ('Max', 'Lang', null),
+       ('Adam', 'Rubin', null),
+       ('Rick', 'Riordan', null);
+
+insert into authorbooks(bookid, authorid, dateposted)
+values ('9781338801910', 1, '2018-12-12'),
+       ('9780385376716', 2, '2019-07-19'),
+       ('9780593486924', 3, '2021-02-28'),
+       ('9780593486924', 4, '2021-02-28'),
+       ('9788498385489', 5, '2006-03-18'),
+       ('9780147515599', 6, '2021-02-28');
+
