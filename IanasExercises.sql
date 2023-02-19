a6a20776b9e2a1ed4b1255dd4a8cceb1f9ab0316
Iana Postolachi <iana@shouter.app>
2023-02-19 11:43:58 +0100

    zeros in amount of books sold

--- IanasExercises.sql
+++ IanasExercises.sql
@@ -23,26 +23,43 @@ set city = 'Aarhus' and zipcode = '8000' and street = 'Banegard' and street = 44
 where id = 2;
 
 --ISBN number of the books that have more then one author
-select isbn from book
-join authorbooks a on book.isbn = a.bookid
+select isbn
+from book
+         join authorbooks a on book.isbn = a.bookid
 group by isbn
 having count(isbn) > 1;
 
 --ISBN number of books that sold at least X copies
-select isbn from book
-join type t on book.isbn = t.bookid
-join itemordertype i on t.id = i.typeid
+select isbn
+from book
+         join type t on book.isbn = t.bookid
+         join itemordertype i on t.id = i.typeid
 group by i.typeid, book.isbn
 having i.typeid notnull;
 
 
---Not showing the books that haven't sold any copies((
-
 --Number of copies of each book sold – unsold books should show as 0 sold copies
-select count(isbn) as number_of_books_sold, title from book
+create temporary table books_were_sold as
+select count(isbn) as number_of_books_sold, title
+from book
+         join type t on book.isbn = t.bookid
+         join itemordertype i on t.id = i.typeid
+group by isbn, i.orderid;
+
+create temporary table types_that_not_sold as
+select id
+from type
+    except
+select typeid
+from itemordertype;
+
+select number_of_books_sold, title from books_were_sold
+group by number_of_books_sold,title
+union
+select 0,title from book
 join type t on book.isbn = t.bookid
-join itemordertype i on t.id = i.typeid
-group by isbn;
+join types_that_not_sold n on t.id = n.id;
+
 
 
 
