269fcf7a080a85918d50ed4572b093b0dcfa6866
Iana Postolachi <iana@shouter.app>
2023-02-19 08:57:18 +0100

    moved "condition" to the book types that have it

--- bookStoreSql.sql
+++ bookStoreSql.sql
@@ -1,158 +1,177 @@
 set search_path = amazon_book_store;
 
 
-create table Book(
-    ISBN varchar(20) PRIMARY KEY,
-    title varchar(100) not null ,
+create table Book
+(
+    ISBN        varchar(20) PRIMARY KEY,
+    title       varchar(100) not null,
     description varchar(500),
-    rating numeric(2, 1),
-    language varchar(20)
+    rating      numeric(2, 1),
+    language    varchar(20)
 );
 
 -- alter table Book alter column title type varchar(100);
 
-create table Author(
-    id serial PRIMARY KEY,
-    firstName varchar(50) not null ,
-    lastname varchar(50) not null,
+create table Author
+(
+    id         serial PRIMARY KEY,
+    firstName  varchar(50) not null,
+    lastname   varchar(50) not null,
     middleName varchar(50)
 );
 -- drop table AuthorBooks;
-create table AuthorBooks(
-    bookId varchar(20),
-    authorId int,
+create table AuthorBooks
+(
+    bookId     varchar(20),
+    authorId   int,
     datePosted date,
-    foreign key (authorId) references Author(id),
-    foreign key (bookId) references Book(ISBN)
+    foreign key (authorId) references Author (id),
+    foreign key (bookId) references Book (ISBN)
 );
 
-create table Character(
-    id serial primary key,
-    name varchar (100)
+create table Character
+(
+    id   serial primary key,
+    name varchar(100)
 );
 
-create table BookCharacters(
-    bookId varchar(20),
+create table BookCharacters
+(
+    bookId      varchar(20),
     characterId int,
-    foreign key (bookId) references Book(ISBN),
-    foreign key (characterId) references Character(id)
+    foreign key (bookId) references Book (ISBN),
+    foreign key (characterId) references Character (id)
 );
 -- drop table Customer;
 
-create table Customer(
-    id serial primary key ,
-    firstName varchar (30),
-    lastName varchar (30),
-    middleName varchar (30),
-    city varchar(50),
-    zipCode varchar(10),
-    country  varchar(30),
-    street varchar (50),
+create table Customer
+(
+    id           serial primary key,
+    firstName    varchar(30),
+    lastName     varchar(30),
+    middleName   varchar(30),
+    city         varchar(50),
+    zipCode      varchar(10),
+    country      varchar(30),
+    street       varchar(50),
     streetNumber varchar(15)
 );
-alter table customer alter column streetNumber type varchar(15);
+alter table customer
+    alter column streetNumber type varchar(15);
 
 
 -- drop  table Type cascade;
---I suggest to put 'condition' inside those book types that have condition 'Hardcover', 'Paperback'
--- cause Audio and others don't have condition
--- Also price is different for all types so 'price' should be included in different book types
-create table Type(
-    id serial primary key ,
-    bookId varchar(20),
-    publisher varchar(50),
+create table Type
+(
+    id          serial primary key,
+    bookId      varchar(20),
+    publisher   varchar(50),
     publishDate date,
-    price numeric(6,2),
-    condition varchar(20),
+    price       numeric(6, 2),
 
-    foreign key (bookId) references Book(ISBN)
+    foreign key (bookId) references Book (ISBN)
 
 );
 
 -- drop table CustomerOrder;
 
-create table CustomerOrder(
-    id serial primary key,
-    typeId  int  references Type(id) ,
-    customerId int references Customer(id),
-    date date,
-    totalPrice numeric(8,2)
+create table CustomerOrder
+(
+    id         serial primary key,
+    typeId     int references Type (id),
+    customerId int references Customer (id),
+    date       date,
+    totalPrice numeric(8, 2)
 );
 
-create table Genre(
-    id serial primary key ,
+create table Genre
+(
+    id   serial primary key,
     name varchar(30)
 );
 
 
 
-create table Category(
-    id serial primary key ,
-    name varchar(30),
-    parentCategoryId int references Category(id)
+create table Category
+(
+    id               serial primary key,
+    name             varchar(30),
+    parentCategoryId int references Category (id)
 );
 -- drop table bookcategory;
-create table BookCategory(
-    bookId varchar(20) references Book(ISBN),
-    categoryId int references Category(id)
+create table BookCategory
+(
+    bookId     varchar(20) references Book (ISBN),
+    categoryId int references Category (id)
 );
-create table BookGenre(
-    bookId varchar(20) references Book(ISBN),
-    genreId int references Genre(id)
+create table BookGenre
+(
+    bookId  varchar(20) references Book (ISBN),
+    genreId int references Genre (id)
 );
 
 
 -- drop table AudioBook;
-create table AudioBook(
-    id int  primary key  references Type(id),
-    listeningTimeMinutes numeric(7,2),
-    narrator varchar(50)
-) ;
+create table AudioBook
+(
+    id                   int primary key references Type (id),
+    listeningTimeMinutes numeric(7, 2),
+    narrator             varchar(50)
+);
 
 -- drop table AudioCD;
 
-create table AudioCD(
-    id int  primary key  references Type(id),
+create table AudioCD
+(
+    id              int primary key references Type (id),
 
     availableCopies int
-) ;
+);
 
 -- drop table Kindle;
 
-create table Kindle(
-    id int  primary key  references Type(id),
-    fileSize varchar(15),
+create table Kindle
+(
+    id          int primary key references Type (id),
+    fileSize    varchar(15),
     printLength int
-) ;
+);
 
 -- drop table HardCover;
 
-create table HardCover(
-    id int  primary key  references Type(id),
+create table HardCover
+(
+    id              int primary key references Type (id),
     availableCopies int,
-    printLength int
-) ;
+    printLength     int,
+
+    condition   varchar(20)
+);
 
 -- drop table PaperBack;
 
-create table PaperBack(
-    id int  primary key  references Type(id),
+create table PaperBack
+(
+    id              int primary key references Type (id),
     availableCopies int,
-    printLength int
+    printLength     int,
+    condition   varchar(20)
 );
 
-create table ItemOrder(
-    id serial primary key ,
-    orderDate date,
-    totalprice numeric(10,2),
+create table ItemOrder
+(
+    id         serial primary key,
+    orderDate  date,
+    totalprice numeric(10, 2),
     customerId int,
-    foreign key (customerId) references Customer(id)
+    foreign key (customerId) references Customer (id)
 );
 
 -- drop table ItemOrderType;
-create table ItemOrderType(
-    typeId int references Type(id),
-    orderId int references ItemOrder(id)
+create table ItemOrderType
+(
+    typeId  int references Type (id),
+    orderId int references ItemOrder (id)
 --     primary key (typeId, orderId)
 )
 
